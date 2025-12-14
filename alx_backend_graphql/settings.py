2b8e395504552f86fe1549463d6cbbcddfc4b26f
joel odunoye <joelodunoye@gmail.com>
2025-12-14 15:00:26 +0100

    update

--- alx_backend_graphql/settings.py
+++ alx_backend_graphql/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crm',
-    'graphene-django',
     'django-filters',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
